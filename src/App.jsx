dc1946f0d8a8dacf6c34dfc44fd24ab45d041710
Andre <arc.xoc@gmail.com>
2022-01-14 09:43:18 -0800

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,17 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  //create a state to store the cat image
-  const [cat, setCat] = useState('https://cataas.com/cat');
-  const [loading, setLoading] = useState('Obtain Cat');
-
-  async function handleClick() {
-    console.log('clicked');
-    setLoading('Loading...');
-    //run an animation to change the cat image
-
-    //get a new cat image from cataas without caching
-
-    let response = await fetch('https://cataas.com/cat', {
-      cache: 'no-store',
-    });
-    var urlCreator = window.URL || window.webkitURL;
-    let imgUrl = urlCreator.createObjectURL(await response.blob());
-    console.log(imgUrl);
-    setCat(imgUrl);
-    setLoading('Obtain Cat');
-  }
-
   // Return the App component.
   return (
     <div className="h-screen bg-zinc-800">
-      <div className="flex items-center justify-center">
-        <div className="flex flex-col h-screen justify-center space-y-5">
-          <div>
-            <img
-              className="text-white rounded-xl border-white border-2"
-              src={cat}
-              alt="kitten"
-            />
-          </div>
-          <div className="flex justify-center">
-            <button
-              className="mt-4 text-white bg-gradient-to-br from-orange-600 to-pink-600 rounded-2xl p-2 text-xl hover:text-2xl transition-all active:text-lg border-2 border-white justify-self-center"
-              onClick={handleClick}
-            >
-              {loading}
-            </button>
-          </div>
+      <div>
+        <div className="">
+          <p className="text-white">Input</p>
+          <input type="text" className="text-white" />
+        </div>
+        <div className="mt-4">
+          <p className="text-white">Process Config</p>
+          <input type="text" className="text-white" />
         </div>
       </div>
     </div>
