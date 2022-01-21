47666286590cbca171058bc91998c74ac96ed572
Brody Bird <Birdlkjlkj123@gmail.com>
2022-01-21 09:48:50 -0800

    Added All Buttons and Boxes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,26 @@ function App() {
   // Return the App component.
   return (
     <div className="h-screen bg-zinc-800">
-      <div>
+      <div class="flex justify-around">
         <div className="">
           <p className="text-white">Input</p>
           <input type="text" className="text-white" />
         </div>
-        <div className="mt-4">
+        <div className="">
           <p className="text-white">Process Config</p>
           <input type="text" className="text-white" />
+        </div>
+        <div className="">
+          <p className="text-white">Output</p>
+          <input type="text" className="text-white" />
+        </div>
+        <div>
+          <button
+            class="text-white border rounded-lg p-3 mt-4"
+            onClick={() => setonClick(true)}
+          >
+          Calculate
+          </button>
         </div>
       </div>
     </div>
